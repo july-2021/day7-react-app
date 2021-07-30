60b1bb1fb6140e2199a17aa35d269c612de0db8b
santosh <santosh.ece06@gmail.com>
2021-07-30 13:27:50 +0530

    props.concept.

--- src/App.js
+++ src/App.js
@@ -1,23 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <MyComponent heading="Java" desc="Java is cool." />
+      <MyComponent heading="Javascript" desc="Javascript is Hot!!" />
+      <MyComponent heading="Python" desc="Python is Intelligent!!" />
+    </div>
+  );
+}
+
+// Reading the PROPS passed by the Parent.
+function MyComponent({ heading, desc }) {
+  return (
+    <div className="bg-dark p-3 text-light mb-1">
+      <h1>Learning {heading}</h1>
+      <hr />
+      <p>
+        <mark className="rounded alert-warning">{desc}</mark> Lorem ipsum dolor
+        sit amet consectetur adipisicing elit. Earum harum iusto, repellendus
+        officia cumque officiis amet sed aspernatur rem assumenda recusandae
+        odio ipsa libero a voluptas voluptate, facere reiciendis aliquam?
+      </p>
     </div>
   );
 }
